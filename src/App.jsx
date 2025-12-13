965a3aa4187e353c3943c84f5ae35852448757fa
abhisheky1817 <abhisheky1817@gmail.com>
2025-12-13 18:45:58 +0530

    done cleanup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  )
+}
+
+export default App; // Exporting the App component as the default export of this module to make it available for import in other files.
